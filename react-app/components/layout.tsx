e85db531c96dc1b04319d0543b8e8ad2de752a05
lastnamearya <arya.jigyasu6815@gmail.com>
2020-11-02 22:45:50 +0530

    update: header

--- react-app/components/layout.tsx
+++ react-app/components/layout.tsx
@@ -14,14 +14,12 @@ const Header = () => {
   )
 
   return (
-    <header className="absolute flex w-screen h-20 px-5 py-1 shadow-md place-items-center place-content-start">
+    <header className="absolute flex w-screen h-20 pl-10 py-1 shadow-md place-items-center place-content-start">
       <div className="flex">
         <img src="/hasura_logo.svg" alt="hasura" id="hasura-logo" />
-        <h1 className="pr-8 text-4xl font-medium text-gray-800">
-          Data Dictionary
-        </h1>
+        <h1 className="pr-10 text-3xl font-medium">Data Dictionary</h1>
       </div>
-      <div className="relative inline-block w-10 mr-2 align-middle transition duration-200 ease-in select-none">
+      <div className="relative inline-block w-10 mr-2 align-middle transition duration-200 ease-in select-none pt-2">
         <input
           type="checkbox"
           name="toggle"
@@ -36,7 +34,7 @@ const Header = () => {
         ></label>
       </div>
 
-      <label htmlFor="toggle" className="text-lg text-gray-700">
+      <label htmlFor="toggle" className="text-lg text-gray-700 pt-2">
         Show for specific roles only
       </label>
 
@@ -87,11 +85,11 @@ const NavMenu = () => {
   ]
 
   return (
-    <nav className="w-1/6 p-4 bg-gray-300">
+    <nav className="w-1/4 p-6 bg-gray-300">
       {navItems.map(it => (
         <div
           key={it.route}
-          className="flex w-full h-12 pl-4 mt-6 text-left border-l-4 border-indigo-700 place-items-center"
+          className="flex  w-full h-12 mt-6 text-center place-items-center cursor-pointer bg-white shadow-md justify-center rounded delay-75 hover:shadow-lg"
         >
           <Link href={it.route}>
             <p className="text-lg font-medium text-gray-800 hover:text-indigo-700">
